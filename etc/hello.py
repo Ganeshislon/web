6b0aa09096ff018265dea2b94e8d8ab413965cf6
Ganeshislon <nkopaew@ya.ru>
2017-07-21 18:46:19 +0300

    Create hello.py

---
+++ etc/hello.py
@@ -0,0 +1,9 @@
+pythonpath = /home/box/web/
+python = hello:application
+bind = "0.0.0.0:8080"
+workers = 5
+user = "www"
+group = "www"
+logfile = "/home/proft/projects/blog/log/gunicorn.log"
+loglevel = "info"
+proc_name = "blog"
